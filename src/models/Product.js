98ddab254ffb5022d46f57b7dd4569b04843f775
HE170693HoNgocKhanh <khanhhhnhe170693@fpt.edu.vn>
2025-10-23 01:25:48 +0700

    feat: add model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,70 +1,69 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
-    },
-    image: {
-      type: String,
-    },
-    countInStock: {
-      type: Number,
-      required: true,
-      min: 0,
-      default: 0,
-    },
-    rating: {
-      type: Number,
-      default: 0,
-      min: 0,
-      max: 5,
-    },
-    numReviews: {
-      type: Number,
-      default: 0,
-    },
-    variants: [
-      {
-        name: String,
-        image: String,
-        stock: Number,
-      },
-    ],
-    personalize: {
-      type: Boolean,
-      default: false,
-    },
-    seller: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true,
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  image: {
+    type: String
+  },
+  images: [{
+    type: String
+  }],
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5
+  },
+  numReviews: {
+    type: Number,
+    default: 0
+  },
+  variants: [{
+    size: String,
+    color: String,
+    price: Number,
+    countInStock: Number
+  }],
+  personalize: {
+    type: Boolean,
+    default: false
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
   }
-);
+}, {
+  timestamps: true
+});
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
